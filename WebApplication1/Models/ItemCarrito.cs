cbf31cc54925bbe7e4c1eb2673ec17d1e6e8a6ec
bastiantobar <bastiantobar.94@gmail.com>
2024-02-20 19:24:43 +0100

    se agregan modelo y data dummy

---
+++ WebApplication1/Models/ItemCarrito.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class ItemCarrito
+    {
+        public int ProductoId { get; set; }
+        public int Cantidad { get; set; }
+    }
+}
---
+++ WebApplication1/Models/Pedido.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Pedido
+    {
+        public int PedidoId { get; set; }
+        public List<ItemCarrito> Items { get; set; }
+        public DateTime Fecha { get; set; }
+    }
+}
